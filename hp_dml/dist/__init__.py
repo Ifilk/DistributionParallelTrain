91c6bde1682febfcb19efeed2ae2493b288eedb6
suanxc <suanxc@yeah.net>
2024-11-25 15:15:03 +0800

    feat(dist): 重构通信模块并实现全局通信对象
    - 重构了 DistributionComm 类，优化了发送和接收消息的逻辑
    - 新增了全局通信对象 global_comm 和相关函数（send、recv等）- 更新了 ProcessManager 类，增加了任务队列和互斥任务处理
    - 修改了 Node 类，使用新的全局通信函数替代 DistributionComm 对象

--- hp_dml/dist/__init__.py
+++ hp_dml/dist/__init__.py
@@ -1,3 +1,8 @@
-from .comm import DistributionComm, CommException, CommTimeOutException
+from .comm import (DistributionComm, CommException, CommTimeOutException,
+                   init_process_group, send, recv, last_recv, MetaMessage,
+                   get_meta_state)
 from .node import Node, SendException
 from .parallel import ProcessManager, Task
+
+from torch.distributed import get_rank, is_available, is_nccl_available, \
+                              is_initialized
